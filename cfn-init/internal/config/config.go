1455f0c39e885709565ebac9022d4cba09c508a0
Chris Mendoza <chrisqm@amazon.com>
2025-10-14 17:10:42 -0400

    Add consts and fix config helper methods

--- cfn-init/internal/config/config.go
+++ cfn-init/internal/config/config.go
@@ -1,13 +1,16 @@
 package config
 
 import (
+	"cfn-init/internal/permissions"
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
-// ReadConfigFile loads a project configuration from the specified file path.
-func ReadConfigFile(path string) (*ProjectConfig, error) {
-	data, err := os.ReadFile(path)
+// ReadConfigFile loads a project configuration from the workspace path.
+func ReadConfigFile(workspacePath string) (*ProjectConfig, error) {
+	configPath := filepath.Join(workspacePath, "cfn-project", "cfn-config.json")
+	data, err := os.ReadFile(configPath)
 	if err != nil {
 		return nil, err
 	}
@@ -20,11 +23,12 @@ func ReadConfigFile(path string) (*ProjectConfig, error) {
 	return &config, nil
 }
 
-// WriteConfigFile saves a project configuration to the specified file path.
-func WriteConfigFile(path string, config *ProjectConfig) error {
+// WriteConfigFile saves a project configuration to the workspace path.
+func WriteConfigFile(workspacePath string, config *ProjectConfig) error {
+	configPath := filepath.Join(workspacePath, "cfn-project", "cfn-config.json")
 	data, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(path, data, 0644)
+	return os.WriteFile(configPath, data, permissions.ConfigFile)
 }
