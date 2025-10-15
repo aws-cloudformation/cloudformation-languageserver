76d56753bbcbd15ef1d3aa3126c6f18f754570bc
Satyaki Ghosh <satyakig@amazon.com>
2025-10-14 22:34:05 -0400

    Encrypt LMDB

---
+++ src/utils/MachineKey.ts
@@ -0,0 +1,25 @@
+import { hkdfSync } from 'crypto';
+import { homedir, userInfo, platform, arch, hostname, cpus } from 'os';
+import { ExtensionName } from './ExtensionConfig';
+
+export function stableMachineSpecificKey(salt: string, info: string, keyLen: number = 255): Buffer {
+    // Collect CPU model information, joining all CPU models with hyphens
+    const cpuModel =
+        cpus().length > 0
+            ? cpus()
+                  .map((cpu) => cpu.model)
+                  .join('-')
+                  .trim()
+            : 'unknown-cpu';
+
+    const fingerprintString = [ExtensionName, platform(), arch(), hostname(), userInfo().username, homedir(), cpuModel]
+        .join('-')
+        .replaceAll(/[^a-zA-Z0-9]/g, '');
+
+    const derivedKey = hkdfSync('sha512', fingerprintString, salt, info, keyLen);
+    if (Buffer.isBuffer(derivedKey)) {
+        return derivedKey;
+    } else {
+        return Buffer.from(derivedKey);
+    }
+}
