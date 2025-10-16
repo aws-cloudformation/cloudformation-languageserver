8b1f8519bdf12506f03a0743c76f8bf67acf839f
gemammercado <gemamrcd@amazon.com>
2025-10-16 11:54:26 -0400

    Resolve errors after merging

--- tst/e2e/autocomplete/Autocomplete.test.ts
+++ tst/e2e/autocomplete/Autocomplete.test.ts
@@ -1520,16 +1520,17 @@ Resources:
             };
             template.executeScenario(scenario);
         });
+    });
 
-        it('test nested object property completion', () => {
-            const template = new TemplateBuilder(DocumentType.JSON, '');
-            const scenario: TemplateScenario = {
-                name: 'Nested object property completion',
-                steps: [
-                    {
-                        action: 'type',
-                        position: { line: 0, character: 0 },
-                        content: `{
+    it('test nested object property completion', () => {
+        const template = new TemplateBuilder(DocumentType.JSON, '');
+        const scenario: TemplateScenario = {
+            name: 'Nested object property completion',
+            steps: [
+                {
+                    action: 'type',
+                    position: { line: 0, character: 0 },
+                    content: `{
   "AWSTemplateFormatVersion": "2010-09-09",
   "Resources": {
     "MyBucket": {
@@ -1542,21 +1543,20 @@ Resources:
     }
   }
 }`,
-                        verification: {
-                            position: { line: 7, character: 11 },
-                            expectation: CompletionExpectationBuilder.create()
-                                .expectContainsItems([
-                                    'TopicConfigurations',
-                                    'QueueConfigurations',
-                                    'LambdaConfigurations',
-                                    'EventBridgeConfiguration',
-                                ])
-                                .build(),
-                        },
+                    verification: {
+                        position: { line: 7, character: 11 },
+                        expectation: CompletionExpectationBuilder.create()
+                            .expectContainsItems([
+                                'TopicConfigurations',
+                                'QueueConfigurations',
+                                'LambdaConfigurations',
+                                'EventBridgeConfiguration',
+                            ])
+                            .build(),
                     },
-                ],
-            };
-            template.executeScenario(scenario);
-        });
+                },
+            ],
+        };
+        template.executeScenario(scenario);
     });
 });
