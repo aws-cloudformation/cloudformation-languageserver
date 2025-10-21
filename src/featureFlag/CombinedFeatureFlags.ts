3363c89a505f630971628d38f11c71084be33159
Satyaki Ghosh <satyakig@amazon.com>
2025-10-21 15:04:40 -0400

    Improve feature flag building (#109)

--- src/featureFlag/CombinedFeatureFlags.ts
+++ src/featureFlag/CombinedFeatureFlags.ts
@@ -1,6 +1,6 @@
 import { hostname } from 'os';
 import { FleetTargetedFeatureFlag } from './FeatureFlag';
-import { FeatureFlag } from './FeatureFlagI';
+import { FeatureFlag, TargetedFeatureFlag } from './FeatureFlagI';
 
 export class LocalHostTargetedFeatureFlag implements FeatureFlag {
     private readonly enabled: boolean;
@@ -36,6 +36,25 @@ export class AndFeatureFlag implements FeatureFlag {
 
     describe(): string {
         return this.featureFlags
+            .map((feature) => {
+                return feature.describe();
+            })
+            .join(', ');
+    }
+}
+
+export class CompoundFeatureFlag<T> implements TargetedFeatureFlag<T> {
+    constructor(
+        private readonly featureFlag: FeatureFlag,
+        private readonly targetedFeatureFlag: TargetedFeatureFlag<T>,
+    ) {}
+
+    isEnabled(target: T): boolean {
+        return this.featureFlag.isEnabled() && this.targetedFeatureFlag.isEnabled(target);
+    }
+
+    describe(): string {
+        return [this.featureFlag, this.targetedFeatureFlag]
             .map((feature) => {
                 return feature.describe();
             })
