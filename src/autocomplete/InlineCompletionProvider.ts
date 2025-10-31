f9b3eea4656357fbf3f6dcf223fb076962d50a83
Deep Furiya <79759607+deepfuriya@users.noreply.github.com>
2025-10-31 17:01:45 -0400

    removed inline completion from codebase (#181)
    Co-authored-by: Deep Furiya <dfuriya@amazon.com>

--- src/autocomplete/InlineCompletionProvider.ts
+++
@@ -1,9 +0,0 @@
-import { InlineCompletionItem, InlineCompletionParams } from 'vscode-languageserver-protocol';
-import { Context } from '../context/Context';
-
-export interface InlineCompletionProvider {
-    getInlineCompletion(
-        context: Context,
-        params: InlineCompletionParams,
-    ): Promise<InlineCompletionItem[]> | InlineCompletionItem[] | undefined;
-}
--- src/protocol/LspCapabilities.ts
+++ src/protocol/LspCapabilities.ts
@@ -30,7 +30,6 @@ export const LspCapabilities: InitializeResult = {
                 labelDetailsSupport: true,
             },
         },
-        inlineCompletionProvider: true,
         definitionProvider: true,
         documentSymbolProvider: true,
         executeCommandProvider: {
