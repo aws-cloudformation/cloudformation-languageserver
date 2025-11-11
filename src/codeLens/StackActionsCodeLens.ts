4bf2f70acd03a6dd8f4c782bc574d7114623f352
Zeeshan Ahmed <37942674+Zee2413@users.noreply.github.com>
2025-11-11 16:55:42 -0500

    Enable validation and deployment CodeLens only for templates with res… (#247)

--- src/codeLens/StackActionsCodeLens.ts
+++ src/codeLens/StackActionsCodeLens.ts
@@ -1,4 +1,8 @@
 import { CodeLens, Position, Range } from 'vscode-languageserver';
+import { TopLevelSection } from '../context/ContextType';
+import { getEntityMap } from '../context/SectionContextBuilder';
+import { SyntaxTreeManager } from '../context/syntaxtree/SyntaxTreeManager';
+import { Document } from '../document/Document';
 
 const STACK_ACTION_TITLES = {
     VALIDATE_DEPLOYMENT: 'Validate Deployment',
@@ -10,8 +14,39 @@ const STACK_ACTION_COMMANDS = {
     DEPLOY: 'aws.cloudformation.api.deployTemplate',
 } as const;
 
-export function getStackActionsCodeLenses(uri: string): CodeLens[] {
-    const range = Range.create(Position.create(0, 0), Position.create(0, 0));
+export function getStackActionsCodeLenses(
+    uri: string,
+    document: Document,
+    syntaxTreeManager: SyntaxTreeManager,
+): CodeLens[] {
+    const syntaxTree = syntaxTreeManager.getSyntaxTree(uri);
+    if (!syntaxTree) {
+        return [];
+    }
+
+    const resourcesMap = getEntityMap(syntaxTree, TopLevelSection.Resources);
+    if (!resourcesMap || resourcesMap.size === 0) {
+        return [];
+    }
+
+    if (!document.isTemplate()) {
+        document.updateCfnFileType();
+        if (!document.isTemplate()) {
+            return [];
+        }
+    }
+
+    let codeLensLine = 0;
+    const lines = document.getLines();
+    for (const [i, line] of lines.entries()) {
+        const lineContents = line.trim();
+        if (lineContents.length > 0 && !lineContents.startsWith('#')) {
+            codeLensLine = i;
+            break;
+        }
+    }
+
+    const range = Range.create(Position.create(codeLensLine, 0), Position.create(codeLensLine, 0));
 
     return [
         {
