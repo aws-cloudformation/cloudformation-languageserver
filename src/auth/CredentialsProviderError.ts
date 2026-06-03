1f96801509e35304c6f1cf4eaa38f083440e108a
Matteo Rinaudo <rinaudom@amazon.com>
2026-06-03 15:43:21 -0400

    fix: restore setPrototypeOf for instanceof safety.

--- src/auth/CredentialsProviderError.ts
+++ src/auth/CredentialsProviderError.ts
@@ -2,5 +2,6 @@ export class CredentialsProviderError extends Error {
     constructor(message: string) {
         super(message);
         this.name = 'CredentialsProviderError';
+        Object.setPrototypeOf(this, CredentialsProviderError.prototype);
     }
 }
